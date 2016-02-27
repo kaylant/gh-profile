f695c2605b15497b5337be0b44f58fac1c99ab9a
Kaylan Smith <kaylansmith@Kaylans-MacBook-Pro.local>
2016-02-26 19:06:08 -0600

    working, needs styling

--- js/script.js
+++ js/script.js
@@ -1,74 +1,74 @@
 // console.log($)
-var key = '?access_token=dd41997d9a91b6167e894d9421ff014f810163e7'
-var baseURL = 'https://api.github.com/users/kaylant'
-var profileURL = baseURL + key
-var repoURL = baseURL + '/repos' + key
+// var key = '?access_token=dd41997d9a91b6167e894d9421ff014f810163e7'
+var baseURL = 'https://api.github.com/users/'
+var profileURL = baseURL + 'kaylant'
+var repoURL = profileURL + '/repos'
 
 var profileContainer = document.querySelector('#profileFromJS')
 var repoContainer = document.querySelector('#repoFromJS')
 var inputEl = document.querySelector("input")
-
-// var formatURLparams = function(paramsObj) {
-//     var paramString = ''
-//  	for (var aKey in paramsObj) {
-//         var val = paramsObj[aKey]
-//         paramString += "&" + aKey + "=" + paramsObj[aKey]
-//     }
-//     return paramString.substr(1)
-// }
-
-// request data, pass to handleResults
-
-// var profilePromise = $.getJSON(profileURL)
-// 	console.log(profilePromise)
-// 	profilePromise.then(handleResults) 
+	// inputEl.placeholder = "Search GitHub User"
 
 var githubApiPromise = function(someURL){
 	return $.getJSON(someURL) //return the executed promise
 }
 
-// var repoPromise = function
-
-// var randomUserPRomise = function(dataFromGithub){
-// 	// code here to formuate url
-
-// 	return $.getJSON(someURL)
-// }
-
-//array to HTML
+// array to HTML
 var generateAllReposHTML = function(jsonData) { 
 	var repoArray = jsonData
 	var totalHtmlString = ''
 	for(var i = 0; i < repoArray.length; i++){
 	    var oneRepo = repoArray[i]
-	    console.log(oneRepo) 
+	    // console.log(oneRepo) 
 	    totalHtmlString += generateRepoHTML(oneRepo) 
 	}
-
 	repoContainer.innerHTML = totalHtmlString
 }
 
 // generate HTML
 var generateProfileHTML = function(dataObj) {
-	console.log(dataObj)
 	var htmlString = "<div class='profileStyle'>"
-		htmlString += "<p> Name: " + dataObj.name + "</p>"
+		htmlString +=    "<p> Name: " + dataObj.name + "</p>"
 		htmlString += "</div>"
 	profileContainer.innerHTML = htmlString
 }
-
-
 
 var generateRepoHTML = function(repoObj) {
 	var htmlString =  "<div>"
 	    htmlString +=    "<h3>" + repoObj.name + "</h3>"
 	    htmlString += "</div>"
-	console.log(htmlString)
 	return htmlString
 }
 
+//search 
 
+var search = function(event) {
+	var inputEl = event.target 
+	if(event.keyCode === 13) {
+		// alert("gross!")
+		var userName = inputEl.value
+		inputEl.value = ""
+		location.hash = userName
+	}	
+}
+
+var doSearch = function(userName){
+	// alert("gross!")
+    var profileURL = baseURL + userName 
+    var userNamePromise = $.getJSON(profileURL)
+    userNamePromise.then(generateProfileHTML)
+    var repoURL = baseURL + userName + '/repos'
+    var userRepoPromise = $.getJSON(repoURL)
+    userRepoPromise.then(generateAllReposHTML)
+}
+
+var controller = function(){
+    var userName = location.hash.substring(1)
+    doSearch(userName)
+}
+
+inputEl.addEventListener('keydown',search)
+window.addEventListener('hashchange',controller)
 
 githubApiPromise(profileURL).then(generateProfileHTML)
 githubApiPromise(repoURL).then(generateAllReposHTML)
-
