9b5e243d7ee82171d333c5d99cc4b7ceb8e9b0cd
Kaylan Smith <kaylansmith@Kaylans-MacBook-Pro.local>
2016-02-27 12:20:22 -0600

    done minus date styling

--- js/script.js
+++ js/script.js
@@ -26,29 +26,37 @@ var generateAllReposHTML = function(jsonData) {
 
 // generate HTML
 var generateProfileHTML = function(dataObj) {
-	console.log(dataObj)
+	// console.log(dataObj)
 	var htmlString = "<div class='nameLoginStyle'>"
 		htmlString +=    "<img class='avatarImg' src='" + dataObj.avatar_url + "'/>"
 		htmlString +=    "<h1 class='name'>" + dataObj.name + "</h1>"
 		htmlString +=	 "<p class='login'>" + dataObj.login + "</p>"
 		htmlString += "</div>"
 		htmlString += "<div class='personalDetailsStyle'>"
-		htmlString +=    "<p class='location' class='octicon octicon-location'>" + dataObj.location + "</p>"
-		htmlString +=    "<p class='email'><a href='mailto:" + dataObj.email + "'>" + dataObj.email + "</a></p>"
-		htmlString +=    "<p class='website'><a href='" + dataObj.blog + "'>" + dataObj.blog + "</a></p>"
-		htmlString +=    "<p class='joinedDate'>Joined on " + dataObj.created_at + "</p>"
+		htmlString +=    "<p class='location'><span class='octicon octicon-location'></span>" + dataObj.location + "</p>"
+		htmlString +=    "<p class='email'><span class='octicon octicon-mail'></span><a href='mailto:" + dataObj.email + "'>" + dataObj.email + "</a></p>"
+		htmlString +=    "<p class='website'><span class='octicon octicon-link'></span><a href='" + dataObj.blog + "'>" + dataObj.blog + "</a></p>"
+		htmlString +=    "<p class='joinedDate'><span class='octicon octicon-clock'></span>Joined on " + dataObj.created_at + "</p>"
 		htmlString += "</div>"
 		htmlString += "<div class='followsStyle'>"
-		htmlString +=    "<div class='boxes'><h3>" + dataObj.followers + "</h3></div>"
-		htmlString +=    "<div class='boxes'><h3>" + dataObj.public_gists + "</h3></div>"
-		htmlString +=    "<div class='boxes'><h3>" + dataObj.following + "</h3></div>"
+		htmlString +=    "<div class='boxes'><h3>" + dataObj.followers + "</h3><span class='smallText'>Follower</span></div>"
+		htmlString +=    "<div class='boxes'><h3>" + dataObj.public_gists + "</h3><span class='smallText'>Starred</span></div>"
+		htmlString +=    "<div class='boxes'><h3>" + dataObj.following + "</h3><span class='smallText'>Following</span></div>"
 		htmlString += "</div>"
 	profileContainer.innerHTML = htmlString
 }
 
 var generateRepoHTML = function(repoObj) {
-	var htmlString =  "<div>"
-	    htmlString +=    "<h3>" + repoObj.name + "</h3>"
+	console.log(repoObj)
+	var htmlString =  "<div class='repoObjStyle'>"
+	    htmlString +=    "<h2>" + repoObj.name + "</h2>"
+	    htmlString +=    "<p class='description'>" + repoObj.description + "</p>"
+	    htmlString +=    "<p class='updated'>Updated <time datetime='" + repoObj.updated_at + "'></time>" + repoObj.updated_at + "</p>"
+	    htmlString +=    "<div class='listStatsStyle'>"
+	    htmlString +=    	"<p class='language'>" + repoObj.language + "</p>"
+	    htmlString +=    	"<p class='stats'><span class='octicon octicon-star'></span>" + repoObj.stargazers_count + "</p>"
+	    htmlString +=    	"<p class='stats'><span class='octicon octicon-git-branch'></span>" + repoObj.forks_count + "</p>"
+	    htmlString +=    "</div>"
 	    htmlString += "</div>"
 	return htmlString
 }
